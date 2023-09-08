6283807a54a734da0e7beaae1974128a95349743
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-09-08 11:17:36 -0500

    update

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default HolbertonCourse;
+class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    // Getter for the name attribute
+    get name() {
+        return this._name;
+    }
+
+    // Setter for the name attribute
+    set name(newName) {
+        if (typeof newName === 'string') {
+            this._name = newName;
+        } else {
+            throw new Error('Name must be a string');
+        }
+    }
+
+    // getter for the length attribute
+    get length() {
+        return this._length;
+    }
+
+    // Setter for the length attribute
+    set length(newLength) {
+        if (typeof newLength === 'number') {
+            this._length = newLength;
+        } else {
+            throw new Error('Length must be a number');
+        }
+    }
+
+    // Getter for the students attribute
+    get students() {
+        return this._students;
+    }
+
+    // Setter for the students attribute
+    set students(newStudents) {
+        if (Array.isArray(newStudents)) {
+            this._students = newStudents;
+        } else {
+            throw new Error('Students must be an array of strings');
+        }
+    }
+}

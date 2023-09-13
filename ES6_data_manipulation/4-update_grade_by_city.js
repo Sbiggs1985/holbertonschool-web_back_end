107c4ebb92873ae5b56c937d701b5eaa0a91fb87
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-09-13 10:04:24 -0500

    update

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,2 +1,11 @@
-import getListStudents from "./0-get_list_students.js";
-
+export default function getStudentsByLocation(array, city, grad) {
+  return array
+    .filter((i) => i.location === city)
+    .map((student) => {
+      const gradeI = grad
+        .filter((i) => i.studentId === student.id)
+        .map((x) => x.grade)[0];
+      const grade = gradeI || 'N/A';
+      return { ...student, grade };
+    });
+}
